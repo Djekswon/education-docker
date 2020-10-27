1fcac00a904c6aada12f9e9980fa1c1c6b69033e
evgrig <zhenya-rigin@mail.ru>
2020-10-27 10:16:21 +0300

    Added contacts endpoint

--- apiApp/app.js
+++ apiApp/app.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/contacts', (req, res) => {
+  res.send('Contacts')
+})
+
 app.get("/error", function (req, res){
   res.status(404).send("NotFound");
 });
